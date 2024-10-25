495fe5300bd650ca4d829e5a3169a001c82afcaa
masahiro-hirata-egsys2 <masahiro_01_hirata@jp.honda>
2024-10-25 11:10:23 +0900

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,23 +1,21 @@
-import { Pool } from 'pg';
-import bcrypt from 'bcrypt';
+'use server'
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // 環境変数からデータベースのURLを取得
-});
+import { PrismaClient } from '@prisma/client';
 
-// ユーザーの認証を行う関数
-export const authenticateUser = async (username: string, password: string) => {
-    try {
-        const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-        const user = result.rows[0];
 
-        if (user && await bcrypt.compare(password, user.password)) {
-            return { success: true, user }; // 認証成功
-        } else {
-            return { success: false, message: 'ユーザ名またはパスワードが間違っています' }; // 認証失敗
-        }
-    } catch (error) {
-        console.error('Error authenticating user:', error);
-        throw new Error('サーバーエラー');
+const prisma = new PrismaClient()
+
+export const loginUser = async (name: string, password: string ) => {
+    
+    
+    const user = await prisma.account.findUnique({
+        where: {name: name},
+    });
+
+    if (user && user.password === password){
+        return true;
     }
-};
+    
+    return false;
+    
+}
